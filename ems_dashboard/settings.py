94d689608d4bf78832f607e15591990be98abea9
“Gurpartap2k1” <gurpartapsingh2k1@gmail.com>
2025-08-29 16:40:43 +0530

    new key hidden

--- ems_dashboard/settings.py
+++ ems_dashboard/settings.py
@@ -9,18 +9,19 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%h&a!!ll2#*irbsencpq7qm+px6ahv(ywg@yx#4brfcsm!+j2x'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
